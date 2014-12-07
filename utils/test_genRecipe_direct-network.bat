469816090652c5852c03beb1538f86fa6ef68f38
meltwater2000 <meltwater2000@github.com>
2014-12-07 22:49:04 +0000

    Only generate recipe card
    Moved description from RECIPE_CARD.txt to README.md file.  Seemed
    wasteful to duplicate the information contained within the same folder.

--- utils/test_genRecipe_direct-network.bat
+++ utils/test_genRecipe_direct-network.bat
@@ -5,12 +5,15 @@ set TEXTPAR_PATH="./win-textparser/textparser.exe"
 set DIRECTORY=../sdcard/pi-kitchen/%RECIPE_NAME%
 set RECIPE_DIRECTORY=../recipes
 set TEMPLATE_FILE=TMP_README_RECIPE_CARD.md
-set THETARGETFILE=README.md
+REM set THETARGETFILE=README.md
 set TEMPLATE_FILE_HTML=TMP_RECIPE_CARD.html
 set THETARGETFILE_HTML=%RECIPE_NAME%_RECIPE_CARD.html
 
-%TEXTPAR_PATH% %TEMPLATE_FILE%
-%GPP_PATH% -O "%DIRECTORY%/%THETARGETFILE%" output.txt -I"%DIRECTORY%"
+REM Instead of generating README.md file it will be used
+REM to provide the description for the HTML file, this
+REM will avoid duplicating info.
+REM %TEXTPAR_PATH% %TEMPLATE_FILE%
+REM %GPP_PATH% -O "%DIRECTORY%/%THETARGETFILE%" output.txt -I"%DIRECTORY%"
 
 %TEXTPAR_PATH% %TEMPLATE_FILE_HTML%
 %GPP_PATH% -O "%RECIPE_DIRECTORY%/%THETARGETFILE_HTML%" output.txt -I"%DIRECTORY%"
